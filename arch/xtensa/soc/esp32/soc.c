96867073e91c88a545a6f000b9c56140bcaf6eda
Leandro Pereira <leandro.pereira@intel.com>
2017-09-19 17:30:48 -0700

    arch: esp32: Enable the flash cache
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>

--- arch/xtensa/soc/esp32/soc.c
+++ arch/xtensa/soc/esp32/soc.c
@@ -11,18 +11,129 @@
 #include <xtensa/config/core-isa.h>
 #include <xtensa/corebits.h>
 
+#include <kernel.h>
+#include <misc/util.h>
+#include <soc.h>
 #include <string.h>
 #include <toolchain/gcc.h>
 #include <zephyr/types.h>
 
 extern void _Cstart(void);
 
-/*
- * This is written in C rather than assembly since, during the port bring up,
- * Zephyr is being booted by the Espressif bootloader.  With it, the C stack
- * is already set up.
+static void IRAM_SECTION reset_mmu(void)
+{
+	volatile u32_t *reg;
+
+	esp32_rom_cache_read_disable(0);
+	esp32_rom_cache_read_disable(1);
+
+	esp32_rom_cache_flush(0);
+	esp32_rom_cache_flush(1);
+
+	/* According to esp-idf, toggling the DPORT_APP_CACHE_MMU_IA_CLR bit
+	 * is necessary as a workaround to a hardware bug.  No additional
+	 * details are given as to why this is necessary: the flash cache
+	 * isn't pubicly documented as of September 2017.
+	 */
+	esp32_rom_mmu_init(0);
+	reg = (u32_t *)DPORT_APP_CACHE_CTRL1_REG;
+	*reg |= DPORT_APP_CACHE_MMU_IA_CLR;
+	esp32_rom_mmu_init(1);
+	*reg &= ~DPORT_APP_CACHE_MMU_IA_CLR;
+}
+
+static inline int IRAM_SECTION calc_pages(void *start, void *end)
+{
+	uintptr_t size = (uintptr_t)start - (uintptr_t)end;
+
+	return (int)ROUND_UP(size + 0xFFFF, 0x10000);
+}
+
+static inline u32_t IRAM_SECTION mask_addr(void *addr)
+{
+	return (u32_t)((uintptr_t)addr & 0xffff0000);
+}
+
+static inline u32_t IRAM_SECTION set_up_cache_region(void *start_addr,
+						     void *end_addr)
+{
+	const int pages = calc_pages(start_addr, end_addr);
+	const u32_t addr = mask_addr(start_addr);
+
+	return esp32_rom_cache_flash_mmu_set(0, 0, addr, addr, 64, pages);
+}
+
+static void IRAM_SECTION setup_mmu(void)
+{
+	extern char _rodata_start[], _rodata_end[];
+	extern char _irom_text_start[], _irom_text_end[];
+	volatile u32_t *flash_mmu_tbl = DPORT_PRO_FLASH_MMU_TABLE;
+	int ret;
+	int i;
+
+	for (i = 0; i < DPORT_FLASH_MMU_TABLE_SIZE; i++) {
+		flash_mmu_tbl[i] = DPORT_FLASH_MMU_TABLE_INVALID_VAL;
+	}
+
+	/* FIXME: should the return value of these be ignored?  esp-idf does. */
+	ret = set_up_cache_region(_rodata_start, _rodata_end);
+/*	if (ret) {
+		k_panic();
+	}
+*/
+
+	ret = set_up_cache_region(_irom_text_start, _irom_text_end);
+/*	if (ret) {
+		k_panic();
+	}*/
+	
+}
+
+static void IRAM_SECTION enable_flash_cache_cpu0(void)
+{
+	volatile u32_t *reg;
+
+	reset_mmu();
+
+	/* Unmask the DROM0 on PRO CPU, and ensure the one for APP CPU is
+	 * kept masked while SMP support isn't in place.
+	 */
+	reg = (u32_t *)DPORT_PRO_CACHE_CTRL1_REG;
+	*reg &= ~DPORT_PRO_CACHE_MASK_DROM0;
+
+	reg = (u32_t *)DPORT_APP_CACHE_CTRL1_REG;
+	*reg |= DPORT_APP_CACHE_MASK_DROM0;
+
+	/* At this stage, the flash cache should be disabled.  But, just
+	 * to be sure, disable it before changing the MMU bits.
+	 */
+	esp32_rom_cache_read_disable(0);
+	esp32_rom_cache_flush(0);
+
+	setup_mmu();
+
+	/* Unmask flash cache bits only for PRO CPU at the moment. */
+	reg = (u32_t *)DPORT_PRO_CACHE_CTRL1_REG;
+	*reg &= ~(DPORT_PRO_CACHE_MASK_IRAM0 |
+		DPORT_PRO_CACHE_MASK_IRAM1 |
+		DPORT_PRO_CACHE_MASK_IROM0 |
+		DPORT_PRO_CACHE_MASK_DROM0 |
+		DPORT_PRO_CACHE_MASK_DRAM1);
+
+	reg = (u32_t *)DPORT_APP_CACHE_CTRL1_REG;
+	*reg |= DPORT_APP_CACHE_MASK_IRAM0 |
+		DPORT_APP_CACHE_MASK_IRAM1 |
+		DPORT_APP_CACHE_MASK_IROM0 |
+		DPORT_APP_CACHE_MASK_DROM0 |
+		DPORT_APP_CACHE_MASK_DRAM1;
+
+	esp32_rom_cache_read_enable(0);
+}
+
+/* We have some things set up from the first stage boot loader inside the ESP32
+ * mask ROM.  So write this in C rather than assembly.
  */
-void __attribute__((section(".iram1"))) __start(void)
+void IRAM_SECTION __start(void)
 {
 	volatile u32_t *wdt_rtc_reg = (u32_t *)RTC_CNTL_WDTCONFIG0_REG;
 	volatile u32_t *wdt_timg_reg = (u32_t *)TIMG_WDTCONFIG0_REG(0);
@@ -59,6 +170,9 @@ void __attribute__((section(".iram1"))) __start(void)
 
 	/* Disable CPU1 while we figure out how to have SMP in Zephyr. */
 	*app_cpu_config_reg &= ~DPORT_APPCPU_CLKGATE_EN;
+
+	/* Set up flash cache for PRO CPU. */
+	enable_flash_cache_cpu0();
 
 	/* Start Zephyr */
 	_Cstart();
--- arch/xtensa/soc/esp32/soc.h
+++ arch/xtensa/soc/esp32/soc.h
@@ -12,6 +12,9 @@
 #include <zephyr/types.h>
 #include <stdbool.h>
 
+/* Instructions must be in IRAM while flash cache isn't enabled */
+#define IRAM_SECTION __attribute__((section(".iram1")))
+
 extern int esp32_rom_intr_matrix_set(int cpu_no,
 				     int interrupt_src,
 				     int interrupt_line);
@@ -25,5 +28,16 @@ extern int esp32_rom_gpio_matrix_out(u32_t gpio, u32_t signal_index,
 extern void esp32_rom_uart_attach();
 extern STATUS esp32_rom_uart_tx_one_char(u8_t chr);
 extern STATUS esp32_rom_uart_rx_one_char(u8_t *chr);
+
+extern void esp32_rom_mmu_init(int cpu_no);
+extern void esp32_rom_cache_flush(int cpu_no);
+extern void esp32_rom_cache_read_enable(int cpu_no);
+extern void esp32_rom_cache_read_disable(int cpu_no);
+extern int esp32_rom_cache_flash_mmu_set(int cpu_no,
+					 int pid,
+					 unsigned int virt_addr,
+					 unsigned int phys_addr,
+					 int page_size,
+					 int num_pages);
 
 #endif /* __SOC_H__ */
